e7c92d0539bce1fb0c908800a81955ace4474bc3
mohitagrawal-crest <mohitagrawal@crestdatasya.com>
2020-04-13 10:50:40 +0530

    Add urls.py

---
+++ AppIQ/Service/consul/urls.py
@@ -0,0 +1,8 @@
+# Consul URLs
+
+AUTH                    = '{}/v1/agent/members'
+CATALOG_NODES           = '{}/v1/catalog/nodes'
+NODE_SERVICES           = '{}/v1/catalog/node-services/{}'
+NODE_CHECK              = '{}/v1/health/node/{}'
+SERVICE_CHECK           = '{}/v1/health/checks/{}'
+SERVICE_INFO            = '{}/v1/catalog/service/{}'
