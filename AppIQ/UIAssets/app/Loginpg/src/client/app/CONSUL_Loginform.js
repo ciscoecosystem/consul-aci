f7ccc30846b7eba96176a2adc697d13d1d5a6b33
yash parihar <yash.parihar@crestdatasys.com>
2020-04-15 21:01:51 +0530

    CAAP-116: feature / action (update,read,delete, write) on agentlist api integration

--- AppIQ/UIAssets/app/Loginpg/src/client/app/CONSUL_Loginform.js
+++ AppIQ/UIAssets/app/Loginpg/src/client/app/CONSUL_Loginform.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 // import { strictEqual } from 'assert';
 // import { stringify } from 'querystring';
-import {Table,Button, Icon, Input, Label, Radio, Tooltip} from "blueprint-react"
+import {Table,Button, Icon, Input, Label, Radio, Tooltip, Loader} from "blueprint-react"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -10,6 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 // Error messages
 const FILL_DETAILS = 'Please fill the details';
 const CANT_DELETE_AGENT_AS_SOME_BEEN_WRITTEN = "An agent is being written, can't process this!";
+const QUERY_URL="http://127.0.0.1:5000/graphql.json";
 
 
 function getCookieVal(offset) {
@@ -46,6 +47,7 @@ function getCookie(name) {
 * @return {string} The response received from portal
 */
 function httpGet(theUrl, payload) {
+        console.log("In httpget request.......");
     window.APIC_DEV_COOKIE = getCookie("app_Cisco_AppIQ_token");
     window.APIC_URL_TOKEN = getCookie("app_Cisco_AppIQ_urlToken");
     var xmlHttp = new XMLHttpRequest();
@@ -97,21 +99,29 @@ class CONSUL_LoginForm extends React.Component {
         }
         */
 
-        this.handleSignIn = this.handleSignIn.bind(this);
         this.notify = this.notify.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.removeAgent = this.removeAgent.bind(this);
         this.addAgent = this.addAgent.bind(this);
+        this.readAgentsCall = this.readAgentsCall.bind(this);
+        this.removeAgentCall = this.removeAgentCall.bind(this);
+        this.addAgentCall = this.addAgentCall.bind(this);
 
         this.state = {
-                details : [{"protocol" : "http", "ip" : "10.0.0.0", "port" : 8050, "token" : "lnfeialilsacirvjlnlaial", "status" : false},
-                {"protocol" : "https", "ip" : "10.0.0.1", "port" : 8051, "token" : "lnfeialilsacirvjlglaial", "status" : true},
-                {"protocol" : "http", "ip" : "10.0.0.1", "port" : 8051, "token" : "lnfeialilsacirvjhnlaial", "status" : false}
-                ],
+                details : [],
             editAgentIndex: null, // [0,1,...n] indicates row number & [-1] indicates a row is added and being written
             editDetailCopy: undefined,
             isNewAgentAdded: false,
+            readAgentLoading: false
         }
+    }
+
+    componentDidMount() {
+        console.log("Component Did mount loginform");  
+        let thiss = this;  
+
+        this.setState({ readAgentLoading: true});  
+        setTimeout(function(){ thiss.readAgentsCall() }, 0 ); // making async function.. fix for now
     }
 
     notify(message) {
@@ -120,60 +130,170 @@ class CONSUL_LoginForm extends React.Component {
           });
     }
 
-    handleSignIn(e) {
-        e.preventDefault()
+    readAgentsCall() {
+        this.setState({ readAgentLoading: true})
+        const payload = {query: `query{
+            ReadCreds{creds}
+        }`}
+
+        try {
+            // let url = document.location.origin + "/appcenter/Cisco/AppIQ/graphql.json";
+            let checkData = httpGet(QUERY_URL, payload);
+            console.log("== > cehckdata", checkData );
+
+            let credsData = JSON.parse(JSON.parse(checkData).data.ReadCreds.creds);
+            if(credsData.status_code == "200") {
+                this.setState({ details: credsData.payload })
+            } else {
+                this.notify("Something went wrong")
+            }
+        }
+        catch(e) {
+            console.log('Error getting agents');
+        }
+        finally {
+            this.setState({ readAgentLoading: false})
+        }
+
+    }
+
+    addAgentCall(agentDetail) {
+        let { isNewAgentAdded } = this.state;
+        
+        delete agentDetail.status;
+        agentDetail.port = parseInt(agentDetail.port);
+        console.log("agentDetail", agentDetail);
+
         window.APIC_DEV_COOKIE = getCookie("app_Cisco_AppIQ_token");
         window.APIC_URL_TOKEN = getCookie("app_Cisco_AppIQ_urlToken");
-        let username = this.refs.username.value;
-        let password = this.refs.password.value;
-        let account = this.refs.account.value;
-        let ip = this.refs.ip.value;
-        let port = this.refs.port.value;
-        let signin = this.props.onSignIn;
-        let signout = this.props.onSignOut;
-        let modal_alert = this.props.alert;
-        var payload = {
-            query: `query{
-  LoginApp(ip:"` + ip + `",port:"` + port + `",username:"` + username + `",account:"` + account + `",password:"` + password + `"){
-    loginStatus
-  }
-}`}
+
+        let payload = {};
+        
+        if (isNewAgentAdded){
+            payload = { query: `query{
+                WriteCreds(agentList: ${JSON.stringify(JSON.stringify([agentDetail]))} ){creds}
+            }`}
+        } else {
+            let editDetailCopy  = Object.assign({}, this.state.editDetailCopy);
+            delete editDetailCopy.status;
+            delete editDetailCopy.token;
+            editDetailCopy.port = parseInt(editDetailCopy.port);
+
+            let dataInput = {
+                "oldData" : editDetailCopy,
+                "newData" : agentDetail
+            }
+
+            payload = { query: `query{
+                UpdateCreds(updateInput: ${JSON.stringify(JSON.stringify(dataInput))}){creds}
+            }`}
+        }
+
         let xhr = new XMLHttpRequest();
-        let url = document.location.origin + "/appcenter/Cisco/AppIQ/graphql.json";
+        // let url = document.location.origin + "/appcenter/Cisco/AppIQ/graphql.json";
         let thiss = this;
         try {
-            xhr.open("POST", url, false);
+            xhr.open("POST", QUERY_URL, false);
             xhr.setRequestHeader("Content-type", "application/json");
             xhr.setRequestHeader("DevCookie", window.APIC_DEV_COOKIE);
             xhr.setRequestHeader("APIC-challenge", window.APIC_URL_TOKEN);
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     let json = JSON.parse(xhr.responseText);
+                    console.log("response json ", json);
+
                     if ('errors' in json) {
-                        thiss.notify("Could not login. The query may be invalid.");
-                        signout();
+                        thiss.notify("Could not Fetch. The query may be invalid.");  
                     }
-                    else if ('loginStatus' in json.data.LoginApp) {
-                        let resp = JSON.parse(json.data.LoginApp.loginStatus)
+                    else if ( ('WriteCreds' in json.data && 'creds' in json.data.WriteCreds ) ||
+                                ('UpdateCreds' in json.data && 'creds' in json.data.UpdateCreds )
+                    ) {
+                        let resp = (isNewAgentAdded) ? JSON.parse(json.data.WriteCreds.creds) : JSON.parse(json.data.UpdateCreds.creds) ;
+
                         if(resp.status_code == 200) {
-                            signin(username, password, account, port, ip);
-                            window.location.href = "app.html";
+                            console.log("Its 200; alling readagentCalls");
+                            toast.success("Agent added successfully", {
+                                position: toast.POSITION.BOTTOM_CENTER
+                            });
+                            thiss.readAgentsCall();
                         }
                         else {
                             thiss.notify(resp.message);
-                            signout();
                         }
                     }
                 }
                 else {
                     thiss.notify("Error while reaching the container.")
-                    signout();
                 }
             }
             xhr.send(JSON.stringify(payload));
         }
         catch(e) {
             this.notify("Error while logging in.");
+            this.setState({ readAgentLoading: false})
+        }
+      
+    }
+
+    removeAgentCall(agentDetail) {
+        delete agentDetail.status;
+        agentDetail.port = parseInt(agentDetail.port);
+        console.log("agentDetail", agentDetail);
+
+        window.APIC_DEV_COOKIE = getCookie("app_Cisco_AppIQ_token");
+        window.APIC_URL_TOKEN = getCookie("app_Cisco_AppIQ_urlToken");
+
+        // let payload = { query: `query{
+        //     WriteCreds(agentList: ${JSON.stringify(JSON.stringify([agentDetail]))} ){creds}
+        // }`}
+        // { "query": 'query{DeleteCreds(agentData:){message}}'}
+        let payload = { query: `query{
+            DeleteCreds(agentData: ${JSON.stringify(JSON.stringify(agentDetail))} ){message}
+        }`}
+        console.log("==> patload ", payload);
+
+        let xhr = new XMLHttpRequest();
+        // let url = document.location.origin + "/appcenter/Cisco/AppIQ/graphql.json";
+        let thiss = this;
+        try {
+            xhr.open("POST", QUERY_URL, false);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.setRequestHeader("DevCookie", window.APIC_DEV_COOKIE);
+            xhr.setRequestHeader("APIC-challenge", window.APIC_URL_TOKEN);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    let json = JSON.parse(xhr.responseText);
+                    console.log("response json ", json);
+
+                    if ('errors' in json) {
+                        thiss.notify("Could not Fetch. The query may be invalid.");   
+                    }
+                    else if ('message' in json.data.DeleteCreds) {
+                        let resp = JSON.parse(json.data.DeleteCreds.message)
+                        if(resp.status_code == 200) {
+                            console.log("Its 200; working delete call");
+                            toast.success("Agent "+agentDetail.ip+" removed successfully", {
+                                position: toast.POSITION.BOTTOM_CENTER
+                            });
+                            thiss.readAgentsCall();
+                        }
+                        else {
+                            thiss.notify(resp.message);
+                        }
+                    }
+                    else {
+                        thiss.notify("Something went wrong!")
+                    }
+                }
+                else {
+                    thiss.notify("Error while reaching the container.")
+                }
+            }
+            xhr.send(JSON.stringify(payload));
+        }
+        catch(e) {
+            this.notify("Error while logging in.");
+            this.setState({ readAgentLoading: false})
         }
     }
 
@@ -192,10 +312,21 @@ class CONSUL_LoginForm extends React.Component {
             this.notify(CANT_DELETE_AGENT_AS_SOME_BEEN_WRITTEN);
             return;
         }
+        
         let newDetails = [...details];
+        if (isNewAgentAdded === true){
+            newDetails.splice(index, 1);
+            this.setState({ details: newDetails, editAgentIndex: null, editDetailCopy: undefined, isNewAgentAdded: false  });
+        }
+       
+        this.setState({ readAgentLoading: true});
+        let thiss = this;
+        setTimeout(function(){ 
+                thiss.removeAgentCall(Object.assign({}, newDetails.splice(index, 1)[0] ) ) 
+        }, 0)
 
-        newDetails.splice(index, 1);
-        this.setState({ details: newDetails, editAgentIndex: null, editDetailCopy: undefined, isNewAgentAdded: false  });
+        // newDetails.splice(index, 1);
+        // this.setState({ details: newDetails, editAgentIndex: null, editDetailCopy: undefined, isNewAgentAdded: false  });
     }
 
     // edit an Agent
@@ -230,16 +361,23 @@ class CONSUL_LoginForm extends React.Component {
         let details = this.state.details[index];
         console.log("update agent", details);
 
-        if (!details.ip || !details.port || !details.token ){ // cgeckung if all field added
+        if (!details.ip || !details.port || !details.token || !details.protocol ){ // cgeckung if all field added
             this.notify(FILL_DETAILS);
             return;
         }
 
-        this.setState({
-            editDetailCopy: undefined,
-            editAgentIndex: null,
-            isNewAgentAdded: false,
-        })
+            let thiss = this;
+            this.setState({ readAgentLoading: true}, function() {
+                setTimeout(function(){ 
+                    thiss.addAgentCall(Object.assign({}, details))
+                    
+                    thiss.setState({
+                        editDetailCopy: undefined,
+                        editAgentIndex: null,
+                        isNewAgentAdded: false,
+                    })
+                }, 0)
+            });       
     }
 
     addAgent() {
@@ -265,10 +403,16 @@ class CONSUL_LoginForm extends React.Component {
     const tableColumns = [  {
         header: 'protocol',
         accessor: 'protocol',
-        Cell: props => <span className="radio-grp">
-            <Radio name={"protocol"+props.index} value="http" checked={props.original.protocol==="http"} label="http" disabled={props.index!==editAgentIndex} onChange={(e)=> this.handleChange(props.index, { name: "protocol", value:"http"})} />
-            <Radio name={"protocol"+props.index} value="https" checked={props.original.protocol==="https"} label="https" disabled={props.index!==editAgentIndex} onChange={(e)=> this.handleChange(props.index, { name: "protocol", value:"https"})} />
+        Cell: props => {
+            let { protocol } = props.original;
+        return <span className="radio-grp">
+            <Radio name={"protocol"+props.index} value="http" label="http" 
+                   checked={protocol === "http"} disabled={props.index!==editAgentIndex} onChange={(e)=> this.handleChange(props.index, { name: "protocol", value:"http"})} />
+   
+            <Radio name={"protocol"+props.index} value="https" label="https"
+                   checked={protocol === "https"} disabled={props.index!==editAgentIndex} onChange={(e)=> this.handleChange(props.index, { name: "protocol", value:"https"})} />
         </span>
+        }
     },
     {
         header: 'ip',
@@ -286,7 +430,7 @@ class CONSUL_LoginForm extends React.Component {
         header: 'Token',
         accessor: 'token',
         width:200,
-        Cell: props =>  <Input placeHolder="token" disabled={props.index!==editAgentIndex} value={props.original.token}  name={"token"} onChange={(e) => this.handleChange(props.index, e.target)} />
+        Cell: props =>  <Input placeHolder="token" type={(props.index!==editAgentIndex) ? "password":"text"} disabled={props.index!==editAgentIndex} value={props.original.token}  name={"token"} onChange={(e) => this.handleChange(props.index, e.target)} />
     }, 
     {
         header: 'status',
@@ -337,8 +481,9 @@ class CONSUL_LoginForm extends React.Component {
             <div className="login-form">
             <ToastContainer />
             <center>
-                <Table style={{width:"90%", maxHeight: "35%"}} data={this.state.details} columns={tableColumns} minRows={0} showPagination={false} TheadComponent={props => null}>
-                </Table>
+                {(this.state.readAgentLoading) ? <Loader> loading </Loader> :
+                <Table loading={this.state.readAgentLoading} style={{width:"90%", maxHeight: "35%"}} data={this.state.details} columns={tableColumns} minRows={3} showPagination={false} TheadComponent={props => null}>
+                </Table>}
             </center>
          
             <form onSubmit={(event)=> { event.preventDefault();}}>
