a14d6346a1eb39bce1f8a901c5d3f99f68f4213d
kanaiya tarapara <kanaiya.tarapara@crestdatasys.com>
2020-04-15 18:41:50 +0530

    service tags changed in consul_tree_parser.

--- AppIQ/Service/consul_generate_d3.py
+++ AppIQ/Service/consul_generate_d3.py
@@ -149,7 +149,7 @@ class generateD3Dict(object):
                                 "Service Instance" : service['serviceInstance'],
                                 "Address" : service_address,
                                 "Service Kind" : service['serviceKind'],
-                                "Service Tags" : service['serviceTags'],
+                                "Service Tag" : service['serviceTags'],
                                 "Service Checks" : service['serviceChecks']
                             }
                             ep_dict['children'].append(node_dict)
