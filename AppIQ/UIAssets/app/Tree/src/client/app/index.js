3d1ebdaeec6a9c494c4aa300e35937816402ab76
Yash Parihar <yash.parihar@crestdatasys.com>
2020-05-06 15:58:05 +0530

    CAAP-499 removing / tree: unecessary file, library and code

--- AppIQ/UIAssets/app/Tree/src/client/app/index.js
+++ AppIQ/UIAssets/app/Tree/src/client/app/index.js
@@ -3,11 +3,7 @@ import { render } from 'react-dom';
 import { Loader } from "blueprint-react"
 import TestComponent from './TestComponent.js';
 import Header from './Header.js'
-import { TREE_VIEW_QUERY_PAYLOAD, PROFILE_NAME, INTERVAL_API_CALL } from "../../../../../constants.js"
-// import { dummyData } from './dummydata.js';
-
-// const QUERY_URL="http://127.0.0.1:5000/graphql.json";
-const QUERY_URL = document.location.origin + "/appcenter/Cisco/AppIQ/graphql.json";
+import { TREE_VIEW_QUERY_PAYLOAD, PROFILE_NAME, INTERVAL_API_CALL, QUERY_URL } from "../../../../../constants.js"
 
 var key = 0;
 function getCookieVal(offset) {
@@ -43,19 +39,19 @@ window.APIC_URL_TOKEN = getCookie("app_Cisco_AppIQ_urlToken");
 var headerInstanceName;
 
 function loadingBoxShow() {
-let health = document.getElementById("health-indicators")
-if(health){
-health.style.display= "none";
-}
-  document.getElementById("loading-box").style.display = "block";
+    let health = document.getElementById("health-indicators")
+    if (health) {
+        health.style.display = "none";
+    }
+    document.getElementById("loading-box").style.display = "block";
 }
 
 function loadingBoxHide() {
-	let health = document.getElementById("health-indicators")
-if(health){
-health.style.display= "block";
-}
-  document.getElementById("loading-box").style.display = "none";
+    let health = document.getElementById("health-indicators")
+    if (health) {
+        health.style.display = "block";
+    }
+    document.getElementById("loading-box").style.display = "none";
 }
 
 class App extends React.Component {
@@ -63,17 +59,17 @@ class App extends React.Component {
         super(props);
         this.state = {
             result: {},
-            treedata : undefined,
+            treedata: undefined,
 
             detailsPane: {
                 visible: false,
                 data: {}
-              },
-        
+            },
+
             detailsPage: {
                 visible: false,
                 data: {}
-              },
+            },
             treeApiLoading: false,
             initialTreeRender: 0,
             treeTranslate: { x: 400, y: -60 },
@@ -82,46 +78,46 @@ class App extends React.Component {
 
         this.reload = this.reload.bind(this);
         this.getData = this.getData.bind(this);
-        this.getStaticData = this.getStaticData.bind(this);
+        // this.getStaticData = this.getStaticData.bind(this);
         this.toggleDetailsPane = this.toggleDetailsPane.bind(this);
         this.toggleeDetailsPage = this.toggleeDetailsPage.bind(this);
         this.handleTransitionTree = this.handleTransitionTree.bind(this);
     }
 
-    componentWillMount(){
-      document.body.style.overflow = "scroll"
+    componentWillMount() {
+        document.body.style.overflow = "scroll"
     }
 
-    componentDidMount() { 
-      this.getData();
-      setInterval(this.getData, INTERVAL_API_CALL);
+    componentDidMount() {
+        this.getData();
+        setInterval(this.getData, INTERVAL_API_CALL);
     }
 
     reload() {
         // alert("Reloading");
         loadingBoxShow("block");
         this.handleTransitionTree([400, -60], 1);
-        this.setState({ initialTreeRender: 0 }, function() {
+        this.setState({ initialTreeRender: 0 }, function () {
             this.getData(true);
         })
     }
 
-    toggleDetailsPane(nodeData = undefined){ // if no argument that means details pane to be closed
+    toggleDetailsPane(nodeData = undefined) { // if no argument that means details pane to be closed
         let isOpen = (nodeData !== undefined);
         this.setState({
             detailsPane: {
-              visible: isOpen,
-              data : isOpen ? nodeData : {}
+                visible: isOpen,
+                data: isOpen ? nodeData : {}
             }
         })
     }
 
-    toggleeDetailsPage(nodeData = undefined){ // if no argument that means details pane to be closed
+    toggleeDetailsPage(nodeData = undefined) { // if no argument that means details pane to be closed
         let isOpen = (nodeData !== undefined);
         this.setState({
             detailsPage: {
-              visible: isOpen,
-              data : isOpen ? nodeData : {}
+                visible: isOpen,
+                data: isOpen ? nodeData : {}
             }
         })
     }
@@ -133,36 +129,37 @@ class App extends React.Component {
         })
     }
 
+    /** To run in local
     getStaticData(fullyReload = false) {
         let thiss = this;
         let { detailsPage, detailsPane, treeApiLoading } = this.state;
 
-        if (detailsPage.visible || detailsPane.visible ) return;
+        if (detailsPage.visible || detailsPane.visible) return;
 
         let xhr = new XMLHttpRequest();
 
         try {
-          console.log("opening post")
-          //   xhr.open("POST", QUERY_URL,true);
-            xhr.open('GET', 'treeData.json' , true)
-            xhr.responseType = 'json';  
+            console.log("opening post")
+            //   xhr.open("POST", QUERY_URL,true);
+            xhr.open('GET', 'treeData.json', true)
+            xhr.responseType = 'json';
             xhr.setRequestHeader("Content-type", "application/json");
 
             xhr.onreadystatechange = function () {
                 console.log("Response ====> ", xhr)
-                if (xhr.readyState == 4){
-                    if(xhr.status == 200 || xhr.status == 304) {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200 || xhr.status == 304) {
                         console.log("In 200 or 304 resposnse")
-                        console.log("Tree data", xhr.response, typeof(xhr.response));
+                        console.log("Tree data", xhr.response, typeof (xhr.response));
                         let treedata = xhr.response;
 
-                        if ((JSON.stringify(thiss.state.treedata) !== JSON.stringify(treedata) || fullyReload) && !treeApiLoading){
+                        if ((JSON.stringify(thiss.state.treedata) !== JSON.stringify(treedata) || fullyReload) && !treeApiLoading) {
                             console.log("Tree data taking ");
-                            thiss.setState({ treeApiLoading: true}, function() {
-                                thiss.setState({ treedata, treeApiLoading: false, initialTreeRender: thiss.state.initialTreeRender + 1 }); 
+                            thiss.setState({ treeApiLoading: true }, function () {
+                                thiss.setState({ treedata, treeApiLoading: false, initialTreeRender: thiss.state.initialTreeRender + 1 });
                             })
                         } else {
-                           console.log("Didnt change as before");
+                            console.log("Didnt change as before");
                         }
 
                     }
@@ -175,138 +172,141 @@ class App extends React.Component {
             console.log("sending post")
             xhr.send(null);
         }
-        catch(except) {
-            thiss.setState({ treeApiLoading: false})
+        catch (except) {
+            thiss.setState({ treeApiLoading: false })
             console.log("Cannot fetch data to fetch Tree data.", except);
         }
     }
+    */
 
     getData(fullyReload = false) {
-      let { detailsPage, detailsPane, treeApiLoading } = this.state;
+        let { detailsPage, detailsPane, treeApiLoading } = this.state;
 
-      if (detailsPage.visible || detailsPane.visible) return; // IF any popup is open dont call api
+        if (detailsPage.visible || detailsPane.visible) return; // IF any popup is open dont call api
 
-      let thiss = this;
-      var urlToParse = location.search;
-      let urlParams = {};
-      urlToParse.replace(
-          new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-          function ($0, $1, $2, $3) {
-              urlParams[$1] = $3;
-          }
-      );
-      let result = urlParams;
+        let thiss = this;
+        var urlToParse = location.search;
+        let urlParams = {};
+        urlToParse.replace(
+            new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+            function ($0, $1, $2, $3) {
+                urlParams[$1] = $3;
+            }
+        );
+        let result = urlParams;
 
-      try{
-        result && this.setState({ result });
-      } catch(err){
-        console.error("set state error", err);
-      }
+        try {
+            result && this.setState({ result });
+        } catch (err) {
+            console.error("set state error", err);
+        }
 
-      let payload = TREE_VIEW_QUERY_PAYLOAD(result['tn'], result[PROFILE_NAME])
-      let xhr = new XMLHttpRequest();
+        let payload = TREE_VIEW_QUERY_PAYLOAD(result['tn'], result[PROFILE_NAME])
+        let xhr = new XMLHttpRequest();
 
-      try {
-        console.log("opening post")
-          xhr.open("POST", QUERY_URL,true); 
-          xhr.setRequestHeader("Content-type", "application/json");
-          xhr.setRequestHeader("DevCookie", window.APIC_DEV_COOKIE);
-          xhr.setRequestHeader("APIC-challenge", window.APIC_URL_TOKEN);
-          console.log("header set")
-          xhr.onreadystatechange = function () {
-              if (xhr.readyState == 4){
-                  if(xhr.status == 200) {
-                      let json = JSON.parse(xhr.responseText);
-                      if('errors' in json) {
-                          // Error related to query
-                          localStorage.setItem('message', JSON.stringify(json.errors));
-                          const message_set = true;
-                          window.location.href = "index.html?gqlerror=1";
-                      }
-                      else {
-                          // Response successful
-                          const response = JSON.parse(json.data.OperationalTree.response);
-                          if(response.status_code != "200") {
-                              // Problem with backend fetching data
-                              const message = {"errors": [{
-                                  "message": response.message
-                              }]}
-                              localStorage.setItem('message', JSON.stringify(message.errors));
-                              const message_set = true;
-                              window.location.href = "index.html?gqlerror=1";
-                          }
-                          else {
-                              // Success
-                              var treedata_raw = JSON.parse(json.data.OperationalTree.response).payload;
-                              headerInstanceName = JSON.parse(json.data.OperationalTree.response).agentIP; //  CONSUL : change from instanceName to agentIp
-
-                              if ((JSON.stringify(thiss.state.treedata) !== JSON.stringify(JSON.parse(treedata_raw)) || fullyReload) 
-                                   && !treeApiLoading){
-
-                                console.log("Tree data taking ");
-
-                                thiss.setState({ treeApiLoading: true}, function() {
-                                        thiss.setState({ treedata : JSON.parse(treedata_raw),
-                                                         treeApiLoading: false,
-                                                         initialTreeRender: thiss.state.initialTreeRender + 1 })
-                                })
-
-                              } else {
-                                console.log("Didnt change as before");
-                              }
+        try {
+            console.log("opening post")
+            xhr.open("POST", QUERY_URL, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.setRequestHeader("DevCookie", window.APIC_DEV_COOKIE);
+            xhr.setRequestHeader("APIC-challenge", window.APIC_URL_TOKEN);
+            console.log("header set")
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        let json = JSON.parse(xhr.responseText);
+                        if ('errors' in json) {
+                            // Error related to query
+                            localStorage.setItem('message', JSON.stringify(json.errors));
+                            window.location.href = "index.html?gqlerror=1";
+                        }
+                        else {
+                            // Response successful
+                            const response = JSON.parse(json.data.OperationalTree.response);
+                            if (response.status_code != "200") {
+                                // Problem with backend fetching data
+                                const message = {
+                                    "errors": [{
+                                        "message": response.message
+                                    }]
+                                }
+                                localStorage.setItem('message', JSON.stringify(message.errors));
+                                window.location.href = "index.html?gqlerror=1";
                             }
-                      }
-                  }
-                  else {
-                      // Status code of XHR request not 200
-                      console.log("Cannot fetch data to fetch Tree data.");
-                      if(typeof message_set !== 'undefined') {
-                          const message = {"errors": [{"message": "Error while fetching data for Tree. Status code" + xhr.status}]}
-                          localStorage.setItem('message', JSON.stringify(message.errors));
-                      }
-                      window.location.href = "index.html?gqlerror=1";
-                  }
-              }
-          }
-          console.log("sending post")
-          xhr.send(JSON.stringify(payload));
-      }
-      catch(except) {
-          console.log("Cannot fetch data to fetch Tree data.", except)
-          if(typeof message_set == 'undefined') {
-              const message = {"errors": [{
-                  "message": "Error while fetching data for Tree"
-                }]}
-              localStorage.setItem('message', JSON.stringify(message.errors));
-          }
-          thiss.setState({ treeApiLoading: false})
-          window.location.href = "index.html?gqlerror=1";
-      }
-      key = key + 1;
-      return true;
-  }
+                            else {
+                                // Success
+                                var treedata_raw = JSON.parse(json.data.OperationalTree.response).payload;
+                                headerInstanceName = JSON.parse(json.data.OperationalTree.response).agentIP; //  CONSUL : change from instanceName to agentIp
+
+                                if ((JSON.stringify(thiss.state.treedata) !== JSON.stringify(JSON.parse(treedata_raw)) || fullyReload)
+                                    && !treeApiLoading) {
+
+                                    thiss.setState({ treeApiLoading: true }, function () {
+                                        thiss.setState({
+                                            treedata: JSON.parse(treedata_raw),
+                                            treeApiLoading: false,
+                                            initialTreeRender: thiss.state.initialTreeRender + 1
+                                        })
+                                    })
+
+                                } else {
+                                    console.log("Didnt change as before");
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        // Status code of XHR request not 200
+                        console.log("Cannot fetch data to fetch Tree data.");
+                        if (typeof message_set !== 'undefined') {
+                            const message = { "errors": [{ "message": "Error while fetching data for Tree. Status code" + xhr.status }] }
+                            localStorage.setItem('message', JSON.stringify(message.errors));
+                        }
+                        window.location.href = "index.html?gqlerror=1";
+                    }
+                }
+            }
+            console.log("sending post")
+            xhr.send(JSON.stringify(payload));
+        }
+        catch (except) {
+            console.error("Cannot fetch tree data", except)
+            if (typeof message_set == 'undefined') {
+                const message = {
+                    "errors": [{
+                        "message": "Error while fetching data for Tree"
+                    }]
+                }
+                localStorage.setItem('message', JSON.stringify(message.errors));
+            }
+            thiss.setState({ treeApiLoading: false })
+            window.location.href = "index.html?gqlerror=1";
+        }
+        key = key + 1;
+        return true;
+    }
 
     render() {
         let { treedata, treeApiLoading, initialTreeRender, treeTranslate, treeScale } = this.state;
-      loadingBoxHide();
-      let apptext = " " + this.state.result[PROFILE_NAME]; // CONSUL changes
-      let title = " | Operational"
-      
+        loadingBoxHide();
+        let apptext = " " + this.state.result[PROFILE_NAME]; // CONSUL changes
+        let title = " | Operational"
+
         return (
             <div>
-                <Header text={title} applinktext={apptext} instanceName={headerInstanceName}/>
-                {(treedata === undefined || treeApiLoading) ? <Loader> loading </Loader> : <TestComponent key={key} 
+                <Header text={title} applinktext={apptext} instanceName={headerInstanceName} />
+                {(treedata === undefined || treeApiLoading) ? <Loader> loading </Loader> : <TestComponent key={key}
                     detailsPage={this.state.detailsPage}
                     detailsPane={this.state.detailsPane}
-                    toggleDetailsPane = {this.toggleDetailsPane}
-                    toggleeDetailsPage = {this.toggleeDetailsPage}
-                    data={this.state.treedata} 
-                    reloadController={this.reload} 
+                    toggleDetailsPane={this.toggleDetailsPane}
+                    toggleeDetailsPage={this.toggleeDetailsPage}
+                    data={this.state.treedata}
+                    reloadController={this.reload}
                     datacenterName={this.state.result[PROFILE_NAME]}
                     initialTreeRender={initialTreeRender}
                     treeScale={treeScale}
                     treeTranslate={treeTranslate}
-                    handleTransitionTree={this.handleTransitionTree}/> }
+                    handleTransitionTree={this.handleTransitionTree} />}
             </div>
         );
     }
--- AppIQ/UIAssets/constants.js
+++ AppIQ/UIAssets/constants.js
@@ -1,6 +1,9 @@
 export const PROFILE_NAME = "datacenterName";
 export const INTERVAL_API_CALL = 30000; // in milliseconds
 
+// export const QUERY_URL="http://127.0.0.1:5000/graphql.json"; // for Local
+export const QUERY_URL = document.location.origin + "/appcenter/Cisco/AppIQ/graphql.json"; // for Production
+
 export function DC_DETAILS_QUERY_PAYLOAD(tn, datacenter) {
     return { query: 'query{Details(tn:"' + tn + '", datacenter: "' + datacenter + '"){details}}' }
 }
