bba10135ba903e2774fa2d605615f45125486dd7
Yash Parihar <yash.parihar@crestdatasys.com>
2020-06-02 18:34:34 +0530

    CAAP-534 treesidepanel / EP nonserivce endpoint

--- AppIQ/UIAssets/app/Tree/src/client/app/DetailPanel.js
+++ AppIQ/UIAssets/app/Tree/src/client/app/DetailPanel.js
@@ -35,94 +35,92 @@ function formateDataToChartData(data) {
     }
 }
 
+function nonServiceEndPointEP(data) {
+
+    return Object.keys(data).length > 0 ? (<React.Fragment>
+        <table>
+            <tr>
+                <td width="30%" className="bold-font">MAC</td>
+                <td width="70%" className="bold-font">IP</td>
+            </tr>
+            {Object.entries(data).map(elem => <tr>
+                <td>{elem[0]}</td> <td>{elem[1]}</td>
+            </tr>)}
+        </table>
+    </React.Fragment>) : (<React.Fragment></React.Fragment>)
+}
+
 export default function DetailPanel(props) {
-    
+
     let { summaryPaneIsOpen, summaryDetail } = props;
-    let title = summaryDetail.name;
     console.log("==> Detail pane mounted", summaryDetail);
 
-    // let apicInfoOrder = [
-    // { name: "interface", label: "Interface" },
-    // { name: "ip", label: "ip" },
-    // { name: "mac", label: "mac" },
-    // { name: "epgName", label: "epg" },
-    // { name: "epgHealth", label: "epg Health" },
-    // { name: "ap", label: "Application profile" },
-    // { name: "vrf", label: "Vrf" },
-    // { name: "bd", label: "bd" },
-    // { name: "learningSource", label: "Learning Source" },
-    // { name: "reportingController", label: "reporting Controller" },
-    // { name: "hostingServer", label: "hosting Server" }];
 
-    // let nodeInfoOrder = [{ name: "consulNode", label: "consul Node" },
-    // { name: "nodeChecks", label: "node Checks" }];
+    let appProfileOrder = [{ name: "name", label: "Name" },
+    { name: "App-Health", label: "App-Health" }]
 
-    // let serviceInfoOrder = [{ name: "service", label: "service" },
-    // { name: "serviceChecks", label: "service Checks" },
-    // { name: "serviceInstance", label: "service Instance" },
-    // { name: "Port", label: "port" },
-    // { name: "serviceTags", label: "service Tags" },
-    // { name: "serviceKind", label: "service Kind" },
-    // { name: "serviceNamespace", label: "Namespace" }];
+    // {name:"name", label:"Name"},
+    let epgInfoOrder = [{ name: "name", label: "Name" },
+    { name: "VRF", label: "vrf" },
+    { name: "BD", label: "bd" }]
 
-    let appProfileOrder=[{name:"name", label:"Name"},
-                        { name:"App-Health", label:"App-Health"}]
+    let constractInfoOrder = [{ name: "Consumer", label: "consumer" },
+    { name: "Provider", label: "Provider" }]
 
-                        // {name:"name", label:"Name"},
-    let epgInfoOrder=[{name:"name", label:"Name"},
-                    { name:"VRF", label:"vrf"},
-                        { name:"BD", label:"bd"}]
+    let consulNodeOrder = [{ name: "Node", label: "node" },
+    { name: "Node Checks", label: "Node Checks" },
+    { name: "Reporting Node IP", label: "Reporting Node IP" }]
 
-    let constractInfoOrder=[ { name:"Consumer", label:"consumer"},
-                            { name:"Provider", label:"Provider"}]
+    let consulServiceOrder = [{ name: "Service", label: "Service" },
+    { name: "Address", label: "Address" },
+    { name: "Service Checks", label: "Service Checks" }]
 
-    let consulNodeOrder = [  { name:"Node", label:"node"}, 
-                            { name:"Node Checks", label:"Node Checks"},
-                                { name:"Reporting Node IP", label:"Reporting Node IP"}]
 
-    let consulServiceOrder = [  { name:"Service", label:"Service"}, 
-                            { name:"Address", label:"Address"},
-                            { name:"Service Checks", label:"Service Checks"}]
+    let epInfoOrder = [{ name: "name", label: "Name" },
+    { name: "IP", label: "IP" }, { name: "Interfaces", label: "Interfaces" },
+    { name: "VMM-Domain", label: "VMM-Domain" }]
 
 
 
-    function CollapsePane(title, infoOrder, values=undefined, nodeElem = undefined) {
+
+
+    function CollapsePane(title, infoOrder, values = undefined, nodeElem = undefined) {
         let details = values ? values : summaryDetail;
 
         function allDetailRender(showDetails) {
             return infoOrder.map(function (elem) {
-            let { name, label } = elem
-            let detailValue = showDetails[name];
+                let { name, label } = elem
+                let detailValue = showDetails[name];
 
-            if (name === "nodeChecks" || name === "serviceChecks" || name === "Node Checks" || name==="Service Checks") {
-                let { formattedData, totalCnt } = formateDataToChartData(showDetails[name])
-                detailValue = <PieChartAndCounter data={formattedData} totalCount={totalCnt} />
-            }
-            else if (name === "serviceTags") {
-                detailValue = showDetails[name].map(function (tags) {
-                    return <Label theme={"MEDIUM_GRAYY"} size={"MEDIUM"} border={false}>{tags}</Label>
-                })
-            } else if (name === "interface" || name === "Consumer" || name==="Provider") {
-                detailValue = <ul style={{ listStyleType: "none", paddingLeft: "0px" }}>
-                    {showDetails[name].map(function (infcs) {
-                        return <li>{infcs}</li>
-                    })}
-                </ul>
-            }
+                if (name === "nodeChecks" || name === "serviceChecks" || name === "Node Checks" || name === "Service Checks") {
+                    let { formattedData, totalCnt } = formateDataToChartData(showDetails[name])
+                    detailValue = <PieChartAndCounter data={formattedData} totalCount={totalCnt} />
+                }
+                else if (name === "serviceTags") {
+                    detailValue = showDetails[name].map(function (tags) {
+                        return <Label theme={"MEDIUM_GRAYY"} size={"MEDIUM"} border={false}>{tags}</Label>
+                    })
+                } else if (name === "interface" || name === "Consumer" || name === "Provider") {
+                    detailValue = <ul style={{ listStyleType: "none", paddingLeft: "0px" }}>
+                        {showDetails[name].map(function (infcs) {
+                            return <li>{infcs}</li>
+                        })}
+                    </ul>
+                }
 
-            return <PropertyItem propertyLabel={label} propertyValue={detailValue} />
+                return <PropertyItem propertyLabel={label} propertyValue={detailValue} />
             })
         }
 
         return <CollapsiblePanel id="ccpid1" key="ccpkey1" title={title}>
-            
-            {(nodeElem) ? nodeElem : (Array.isArray(details)) ? 
-                details.map((elemData,ind) => <React.Fragment> {allDetailRender(elemData)} 
-                                    {(details.length-1 !== ind) && <hr/>}
-                                     </React.Fragment> ) : 
-                allDetailRender(details)  
+
+            {(nodeElem) ? nodeElem : (Array.isArray(details)) ?
+                details.map((elemData, ind) => <React.Fragment> {allDetailRender(elemData)}
+                    {(details.length - 1 !== ind) && <hr />}
+                </React.Fragment>) :
+                allDetailRender(details)
             }
-            
+
         </CollapsiblePanel>
     }
 
@@ -130,63 +128,69 @@ export default function DetailPanel(props) {
         summaryDetail.attributes["name"] = summaryDetail["sub_label"];
         switch (summaryDetail.name) {
 
-        case NODE_SERVICE_NAME:
-          return (
-            <React.Fragment>
-                {CollapsePane(summaryDetail.name + " Information", [])}
-                {CollapsePane("service info", [])}
-            </React.Fragment>)
-
-        case NODE_EP_NAME:
-          return (
-            <React.Fragment>
-               {CollapsePane(summaryDetail.name + " Information", [])}
-               {CollapsePane("ep info", [])}
-            </React.Fragment>)
-
-        case NODE_EPG_NAME:
-            console.log("Epg info ", summaryDetail)
-          return (
-            <React.Fragment>
-              {CollapsePane(summaryDetail.name + " Information", epgInfoOrder, summaryDetail.attributes )}
-
-            {('Contracts' in summaryDetail.attributes) &&
-                CollapsePane("Contracts Information", constractInfoOrder, summaryDetail.attributes.Contracts )}
-              
-              {CollapsePane("Consul Nodes", consulNodeOrder, summaryDetail.attributes.Nodes)}
-
-            {/* {('Nodes' in summaryDetail.attributes && summaryDetail.attributes.Nodes.length > 0) &&
-                summaryDetail.attributes.Nodes.map(nodeData => CollapsePane("Consul Nodes", consulNodeOrder, nodeData) )} */}
-
-              {CollapsePane("Consul Services", consulServiceOrder, summaryDetail.attributes.Services_List)}
-
-            {/* {('Services_List' in summaryDetail.attributes && summaryDetail.attributes.Services_List.length > 0) &&
-                summaryDetail.attributes.Services_List.map(serviceData => CollapsePane("Consul Services", consulServiceOrder, serviceData) )} */}
+            case NODE_SERVICE_NAME:
+                return (
+                    <React.Fragment>
+                        {CollapsePane(summaryDetail.name + " Information", [])}
+                        {CollapsePane("service info", [])}
+                    </React.Fragment>)
+   
+            case NODE_EP_NAME:
+                if (summaryDetail.level === "grey") {
+                    delete summaryDetail.attributes.name; // delete name as it does have any
+                    return (
+                    <React.Fragment key="ep-ns">
+                        <PropertyItem propertyLabel={"EP Count"} propertyValue={Object.keys(summaryDetail.attributes).length} />
+                        <PropertyItem propertyLabel={"EP Information"} propertyValue={nonServiceEndPointEP(summaryDetail.attributes)} />
+                    </React.Fragment>) 
+                } else {
+                    return (
+                        <React.Fragment key="ep">
+                            {CollapsePane(summaryDetail.name + " Information", epInfoOrder, summaryDetail.attributes)}
+                            {CollapsePane("Consul Nodes", consulNodeOrder, summaryDetail.attributes)}
+                            {CollapsePane("Consul Services", consulServiceOrder, summaryDetail.attributes.Services_List)}
+                        </React.Fragment>)
+                }
             
-            </React.Fragment>)
 
-        default:
-          return (
-            <React.Fragment>
-              {CollapsePane(summaryDetail.name + " Information", appProfileOrder)}
-            </React.Fragment>)
+            case NODE_EPG_NAME:
+                console.log("Epg info ", summaryDetail)
+                return (
+                    <React.Fragment>
+                        {CollapsePane(summaryDetail.name + " Information", epgInfoOrder, summaryDetail.attributes)}
+
+                        {('Contracts' in summaryDetail.attributes) &&
+                            CollapsePane("Contracts Information", constractInfoOrder, summaryDetail.attributes.Contracts)}
+
+                        {CollapsePane("Consul Nodes", consulNodeOrder, summaryDetail.attributes.Nodes)}
+
+                        {CollapsePane("Consul Services", consulServiceOrder, summaryDetail.attributes.Services_List)}
+                    </React.Fragment>)
+
+            default:
+                return (
+                    <React.Fragment>
+                        {CollapsePane(summaryDetail.name + " Information", appProfileOrder)}
+                    </React.Fragment>)
 
         }
     }
 
+    let title = "";
+    if (summaryDetail.name === "Service") {
+      title = summaryDetail.attributes['Service Instance']
+    } else {
+      title = summaryDetail.sub_label || summaryDetail.label || "Non-service Endpoint";
+    }
+
     return (summaryPaneIsOpen) ? <SummaryPane
         subTitle={""}
-        title={summaryDetail["sub_label"]}
+        title={title}
         closeSummaryPane={() => props.setSummaryIsOpen(false)}
         isOpenScreen={true}
         openScreen={() => props.openDetailsPage(summaryDetail)}
     >
         <div className="" style={{ marginBottom: "80%" }}>
-    
-            {/* {CollapsePane("APIC Information", apicInfoOrder)}
-            {CollapsePane("Consul Node Information", nodeInfoOrder)}
-            {CollapsePane("Consul Service Information", serviceInfoOrder)} */}
-
             {panelToShow()}
         </div>
     </SummaryPane> : <div></div>
