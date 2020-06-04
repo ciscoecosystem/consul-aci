4ea83de2b52b38ee1924f32d2c10b0ea66b8cae4
mohitagrawal-crest <mohitagrawal@crestdatasya.com>
2020-06-04 10:19:20 +0530

    change yaml_utils

--- AppIQ/Service/yaml_utils.py
+++ AppIQ/Service/yaml_utils.py
@@ -1,16 +1,16 @@
-import yaml
+# import yaml
 
 
-def get_conf_value(key_name):
-    """Function to return value from yaml file 
-    with given key name
+# def get_conf_value(key_name):
+#     """Function to return value from yaml file 
+#     with given key name
 
-    Arguments:
-        key_name {str} -- Name of key
+#     Arguments:
+#         key_name {str} -- Name of key
 
-    Returns:
-        type which is defined in yaml file -- Value from yaml file
-    """
-    with open("configuration.yaml", 'r') as stream:
-        data_loaded = yaml.safe_load(stream)
-        return data_loaded.get(key_name)
+#     Returns:
+#         type which is defined in yaml file -- Value from yaml file
+#     """
+#     with open("configuration.yaml", 'r') as stream:
+#         data_loaded = yaml.safe_load(stream)
+#         return data_loaded.get(key_name)
