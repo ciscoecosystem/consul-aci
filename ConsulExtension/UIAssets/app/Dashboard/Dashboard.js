a112739abdfd834e5a626a59e46148917e28ecc6
Parixit Sanghani <parixit.sanghani@CDSYS.LOCAL>
2020-06-16 18:26:22 +0530

    CAAP-880 Fix/replace-giga-to-billion

--- ConsulExtension/UIAssets/app/Dashboard/Dashboard.js
+++ ConsulExtension/UIAssets/app/Dashboard/Dashboard.js
@@ -36,7 +36,7 @@ export default class Dashboard extends React.Component {
 
     nFormatter(num) {
         if (num >= 1000000000) {
-            return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+            return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
         }
         if (num >= 1000000) {
             return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
--- ConsulExtension/UIAssets/app/Tree/src/client/app/utils.js
+++ ConsulExtension/UIAssets/app/Tree/src/client/app/utils.js
@@ -16,7 +16,7 @@ export let  nFormatter = (num) =>{
       return num
     }
     if (num >= 1000000000) {
-        return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+        return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
     }
     if (num >= 1000000) {
         return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
