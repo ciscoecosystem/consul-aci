d9b0862c02ac4413b94d75921d7b39f92ae7571d
nipunbrahmbhatt_crest <nipun.brahmbhatt@crestdatasys.com>
2020-06-19 10:14:21 +0530

    resolved the value error

--- ConsulExtension/Service/plugin_server.py
+++ ConsulExtension/Service/plugin_server.py
@@ -1554,17 +1554,18 @@ def get_nodes_status(ep_ips):
 def get_service_endpoints(ep_ips, service_ips, node_ips):
     """Function to return count of service and non service endpoint
 
-    Args:
-        ep_ips (set): Set of EP IPS
-        service_ips (set): Set of Service IPS
-        node_ips (set): Set og node_ips
+        Args:
+            ep_ips (set): Set of EP IPS
+            service_ips (set): Set of Service IPS
+            node_ips (set): Set og node_ips
 
-    Returns:
-        dict: Count of service and non service endpoint
-    """
-    response = {'service':0, 'non_service':0}
-    consul_ips = service_ips|node_ips
-    common_ips = set()
+        Returns:
+            dict: Count of service and non service endpoint
+        """
+    logger.info('=================== get_service_endpoints =====================')
+    response = {'service': 0, 'non_service': 0}
+    consul_ips = service_ips | node_ips
+    # common_ips = set()
 
     ep_map = {}
     for each in ep_ips:
@@ -1574,7 +1575,12 @@ def get_service_endpoints(ep_ips, service_ips, node_ips):
             else:
                 ep_map[each] = 1
 
-    total_service_count = functools.reduce(lambda a, b: a+b, [v for k, v in ep_map])
+    # for each in ep_ips:
+    #     if each in consul_ips:
+    #         common_ips.add(each)
+    logger.info('======================== service eps ================ ' + str(ep_map))
+    total_service_count = functools.reduce(lambda a, b: a + b, [v for v in ep_map.values()])
+
     response['service'] = total_service_count
     response['non_service'] = len(ep_ips) - total_service_count
     return response
