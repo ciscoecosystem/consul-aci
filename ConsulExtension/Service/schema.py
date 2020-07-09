80f4651405b1268133e7b9868e5476ab3d22e583
meetvalani_crest <meet.valani@crestdatasys.com>
2020-07-09 16:50:41 +0530

    backend support added for ips of non-service eps in expansion view

--- ConsulExtension/Service/schema.py
+++ ConsulExtension/Service/schema.py
@@ -124,6 +124,7 @@ class Query(graphene.ObjectType):
                                         dn=graphene.String(),
                                         mo_type=graphene.String(),
                                         mac_list=graphene.String(),
+                                        ip_list=graphene.String(),
                                         ip=graphene.String())
 
     GetConfiguredAccessPolicies = graphene.Field(GetConfiguredAccessPolicies,
@@ -187,7 +188,7 @@ class Query(graphene.ObjectType):
         return GetAuditLogs
 
     def resolve_GetOperationalInfo(self, info, dn, mo_type, mac_list, ip):
-        GetOperationalInfo.operationalList = app.get_children_ep_info(dn, mo_type, mac_list, ip)
+        GetOperationalInfo.operationalList = app.get_children_ep_info(dn, mo_type, mac_list, ip_list, ip)
         return GetOperationalInfo
 
     def resolve_GetConfiguredAccessPolicies(self, info, tn, ap, epg):
