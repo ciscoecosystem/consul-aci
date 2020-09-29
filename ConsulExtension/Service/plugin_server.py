51700c6ee4895e7b43bf1d2197db9c7da7473454
meetvalani_crest <meet.valani@crestdatasys.com>
2020-09-29 11:31:07 +0530

    removed unrequired logger

--- ConsulExtension/Service/plugin_server.py
+++ ConsulExtension/Service/plugin_server.py
@@ -1044,7 +1044,6 @@ def get_configured_access_policies(tn, ap, epg):
                 tmp_vlan_pool = cap_attr["vLanPool"].split("/from-")[1]
                 vlans = tmp_vlan_pool.split("-to-")
                 vlans = list(map(lambda x:x.split("vlan-")[1][:-1], vlans))
-                logger.info("vlansss {}".format(vlans)) #
                 cap_dict["vlan_pool"] = "{} to {}".format(vlans[0], vlans[1])
             else:
                 logger.error("Attribute {} not found".format("vLanpool"))
